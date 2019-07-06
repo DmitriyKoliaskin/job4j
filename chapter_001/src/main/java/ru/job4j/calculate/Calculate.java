b09548ba3ce29e0b8e613f75763f9b2b2012bdb2
DmitriyKoliaskin <Hipf02@gmail.com>
2019-07-07 02:53:45 +0300

    1.3. JavaDoc. Code convention[#155653]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j.calculate;
-
+/**
+ * Calculate.
+ *
+ * @author Dmitriy Koliaskin (Hipf02@yandex.ru)
+ */ 
 public class Calculate {
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, Job4j.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitriy Koliaskin (Hipf02@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
