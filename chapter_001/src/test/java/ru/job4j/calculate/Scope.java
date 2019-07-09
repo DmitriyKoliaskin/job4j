be0844f456d7097dd5d2522ba757ef61dedf90ee
DmitriyKoliaskin <Hipf02@gmail.com>
2019-07-09 23:52:20 +0300

    3.0. Видео[#155645]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/Scope.java
@@ -0,0 +1,18 @@
+package ru.job4j.calculate;
+
+public class Scope {
+    public static void main(String[] args) {
+
+        int x = 10;
+        if (x == 10) {
+            int y = 20;
+
+            System.out.println(" x и y: " + x + " " + y);
+
+            // y = 100;  Ошибка! переменная y недоступна
+            // в этой области действия, тогда как
+            // переменная x доступна здесь
+            System.out.println(" x равно " + x);
+        }
+    }
+}
