c9f486d04baacda312ac97a802f82961a8496edd
DmitriyKoliaskin <Hipf02@gmail.com>
2019-07-10 03:07:59 +0300

    3.3. Идеальный вес.[#155639]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/Fit.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+/**
+ * Программа расчета идеального веса.
+ */
+public class Fit {
+
+    /**
+     * Иделаьный вес для мужчины.
+     * @param height рост.
+     * @return идеальный вес.
+     */
+    public  double manWeight(double height) {
+        return (height - 100.0) * 1.15;
+    }
+
+    /**
+     * Иделаьный вес для женщины.
+     * @param height рост.
+     * @return идеальный вес.
+     */
+    public  double womanWeight(double height) {
+        return (height - 110.0) * 1.15;
+    }
+}
