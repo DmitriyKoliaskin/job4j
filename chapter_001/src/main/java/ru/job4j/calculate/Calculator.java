1a4278d14fcd70d5db7cc506e0c96be9df91eaee
DmitriyKoliaskin <Hipf02@gmail.com>
2019-07-10 13:43:51 +0300

    3.1. Элементарный калькулятор.[#155643] v01

--- chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
@@ -1,16 +1,40 @@
 package ru.job4j.calculate;
 
-public class Calculator {
+/**
+ * Easy calculator
+ * @author Dmitriy Koliaskin (Hipf02@yandex.ru)
+ */
 
+public class Calculator {
+    /**
+     * @param first value
+     * @param second value
+     * @return first plus second
+     */
     public double add(double first, double second) {
         return first + second;
     }
+    /**
+     * @param first value
+     * @param second value
+     * @return first minus second
+     */
     public double subtract(double first, double second){
         return first - second;
     }
+    /**
+     * @param first value
+     * @param second value
+     * @return first div second
+     */
     public double div(double first, double second){
         return first / second;
     }
+    /**
+     * @param first value
+     * @param second value
+     * @return first multiply second
+     */
     public double multiply(double first, double second){
         return first * second;
     }
