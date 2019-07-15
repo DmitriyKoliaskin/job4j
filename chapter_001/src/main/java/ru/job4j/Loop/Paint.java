7fc6d33c50eaf1aad26ab5589f7f342c43452084
DmitriyKoliaskin <Hipf02@gmail.com>
2019-07-15 12:36:20 +0300

    5.5. Рефакторинг кода.[#155617]

--- chapter_001/src/main/java/ru/job4j/Loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/Loop/Paint.java
@@ -1,5 +1,7 @@
 package ru.job4j.Loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * @author Dmitriy Koliaskin (Hipf02@yandex.ru)
  * @version $Id$
@@ -8,39 +10,31 @@ package ru.job4j.Loop;
 
 public class Paint {
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != height; column++) {
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+       return this.loopyBy(
+               height,
+               height,
+               (row, column) -> row >=column
+       );
     }
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != height; column++) {
-                if (row >= height - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopyBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
     public String pyramid(int height) {
+        return this.loopyBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+    private String loopyBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+            for (int column = 0; column != widht; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
