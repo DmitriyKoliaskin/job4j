08d01d02710cb72129fbada6bad530b9e89ac316
DmitriyKoliaskin <Hipf02@gmail.com>
2019-07-17 22:47:15 +0300

    4.3. Вычисление площади треугольника[#155615] v0.1

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,16 +1,41 @@
 package ru.job4j.condition;
 
+/**
+ * @author Dmitriy Koliaskin (Hipf02@yandex.ru)
+ * @version 0.1
+ */
 public class Triangle {
 
-
-
+    /**
+     * Получаем полупериметр треугольника.
+     * @param a сторона.
+     * @param b сторона.
+     * @param c сторона.
+     * @return полумериметр.
+     */
     public double period(double a, double b, double c) {
         return (a +b + c) / 2;
     }
 
+    /**
+     * Проверка для построения треугольника
+     * @param a сторона.
+     * @param c сторона.
+     * @param b сторона.
+     */
     private boolean exist(double a, double c, double b) {
-        return !(a >= b + c) && !(b >= a + c) && !(c >= b + a);
+        return (a < b + c) && (b < a + c) && (c < b + a);
     }
+
+    /**
+     * Вычисляем площадь треугольника.
+     * @param x1 координата.
+     * @param y1 координата.
+     * @param x2 координата.
+     * @param y2 координата.
+     * @param x3 координата.
+     * @param y3 координата.
+     */
     public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
         double rsl = -1;
         double a = new Point().distance(x1, y1, x2, y2);
