810e190dbdc287412d37d35903dff21bb0a08bb7
DmitriyKoliaskin <Hipf02@gmail.com>
2019-08-12 00:21:45 +0300

    4. Расстояние между точками в трехмерном пространстве.[#155722]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -12,6 +12,10 @@ public class Point {
      * И это поле объекта. Оно доступно только конкретному объекту.
      */
     private int y;
+    /**
+     * Это тоже поле объекта. Оно также доступно только конкретному объекту.
+     */
+    private  int z;
 
     /**
      * Конструктор, который принимает начальное состояние объекта "точка"
@@ -22,10 +26,18 @@ public class Point {
         this.x = first;
         this.y = second;
     }
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
     public void info() {
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 }
