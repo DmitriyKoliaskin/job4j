086369360f739af2589912f3b337467815c88344
DmitriyKoliaskin <Hipf02@gmail.com>
2019-10-01 22:24:11 +0300

    1.6. Взаимодействие объектов. [#166087]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Fox.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Fox {
+    public void tryEat(Ball ball,int position) {
+        if (position == 1) {
+            System.out.println("Я Fox, Ball я тебя Омномном");
+        }
+        else  {
+            System.out.println("Ball, садись ко мне на нос.\nКонец.");
+        }
+    }
+}
