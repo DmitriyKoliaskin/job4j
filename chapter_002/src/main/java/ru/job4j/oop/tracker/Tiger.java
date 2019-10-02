b78927d042efce6319d269bdb6cd9d2f98738ef3
DmitriyKoliaskin <Hipf02@gmail.com>
2019-10-02 22:07:49 +0300

    1. Конструктор по умолчанию. [#173889]

---
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/Tiger.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop.tracker;
+
+public class Tiger extends Predator {
+    public Tiger() {
+        super();
+        System.out.println("Имя класса: Tiger");
+    }
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
