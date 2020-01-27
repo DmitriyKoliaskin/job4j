a155a0ef38f98b232c3a906853c8ad2d0faa6be9
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-01-27 17:54:31 +0300

    0. Что такое инкапсуляция. [#3818]

---
+++ chapter_002/src/main/java/ru/job4j/oop/inheritance/ATM.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop.inheritance;
+
+public class ATM {
+    public static void main(String[] args) {
+        Account account = new Account();
+        account.addMoney(100);
+        account.addMoney(50);
+    }
+}
