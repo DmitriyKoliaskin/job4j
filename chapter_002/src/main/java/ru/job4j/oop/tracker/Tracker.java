634af121f39e1869c63400d82a7f4cdb7f0d3bb9
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-01-27 20:06:38 +0300

    5. Реализовать класс Tracker [#396]

--- chapter_002/src/main/java/ru/job4j/oop/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/Tracker.java
@@ -1,4 +1,87 @@
 package ru.job4j.oop.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указать ячейки для новой заявки.
+     */
+
+    private int position = 0;
+
+    /**
+     * Метод добавления зафки в хранилище
+     * @param item новая заявка
+     */
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для индефикации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    /**
+     * Метод findAll возвращает копию массива this.items без null элементов
+     * @return Массив без null елементов.
+     */
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getName() != null) {
+                itemsWithoutNull[size++] = items[index];
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        return itemsWithoutNull;
+    }
+
+    /**
+     * Метод findByName проверяет в цикле все элементы массива this.items, сравнивая name c key. Все совпадающие элементы собирает в массив.
+     * @param key name, которое ищем среди массива объектов.
+     * @return Массив, в который записаны все элементы с name key.
+     */
+    public Item[] findByName(String key) {
+        Item[] itemsWithSameName = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getName() != null && items[index].getName().equals(key)) {
+                itemsWithSameName[size++] = items[index];
+            }
+        }
+        itemsWithSameName = Arrays.copyOf(itemsWithSameName, size);
+        return itemsWithSameName;
+    }
+
+    /**
+     * Метод findById проверяет в цикле все элементы массива this.items, сравнивая id c key. И выдает этот элемент.
+     * @return Элемент, id которого совпадает с тем который мы ищем.
+     * @param key id, которое ищем среди массива объектов.
+     */
+    public Item findById(String key) {
+        Item result = null;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(key)) {
+                result = items[index];
+                break;
+            }
+        }
+        return result;
+    }
 }
