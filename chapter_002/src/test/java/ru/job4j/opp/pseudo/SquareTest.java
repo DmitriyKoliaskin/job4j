31210aa0c847d56ba0405170a90b0b71308edbe7
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-02-02 18:56:00 +0300

    7. Шаблон проектирования - Стратегия. [#786]

---
+++ chapter_002/src/test/java/ru/job4j/opp/pseudo/SquareTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.opp.pseudo;
+
+import org.junit.Test;
+import ru.job4j.oop.stragery.Square;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class SquareTest {
+    @Test
+    public void squareTesting() {
+        Square square = new Square();
+        assertThat(square.draw(), is(new StringBuilder()
+                .append("+ + + +")
+                .append("+     +")
+                .append("+     +")
+                .append("+ + + +")
+                .toString()));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/opp/pseudo/TriangleTestTwo.java
@@ -0,0 +1,19 @@
+package ru.job4j.opp.pseudo;
+
+import org.junit.Test;
+import ru.job4j.oop.stragery.Triangle;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TriangleTestTwo {
+    @Test
+    public void trianglTest() {
+        Triangle tri = new Triangle();
+        assertThat(tri.draw(), is(new StringBuilder()
+                .append("  *  ")
+                .append(" * * ")
+                .append("*****")
+                .toString()));
+    }
+}
