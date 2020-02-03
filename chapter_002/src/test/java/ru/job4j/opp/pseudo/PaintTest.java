33a70650d72aaafe9aa4f62ec13c745bacfa8aab
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-02-03 08:05:51 +0300

    9.1 Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/opp/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/opp/pseudo/PaintTest.java
@@ -1,21 +1,37 @@
 package ru.job4j.opp.pseudo;
 
-import org.junit.Test;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 import ru.job4j.oop.stragery.Paint;
 import ru.job4j.oop.stragery.Square;
 import ru.job4j.oop.stragery.Triangle;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println(this.stdout);
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("Execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -33,9 +49,6 @@ public class PaintTest {
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
