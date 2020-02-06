52a5c377b16f7fdec05cd3ebb959778d1e155961
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-02-06 21:29:03 +0300

    1.1. Тесты на ValidateInput. [#223313]

--- chapter_002/src/main/java/ru/job4j/oop/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/oop/tracker/ValidateInput.java
@@ -28,7 +28,7 @@ public class ValidateInput extends ConsoleInput {
             } catch (IllegalStateException moe) {
                 System.out.println("Please select key from menu.");
             } catch (NumberFormatException nfe) {
-                System.out.println("Plese enter validate data again.");
+                System.out.println("Please enter validate data again.");
             }
         } while (invalid);
         return value;
