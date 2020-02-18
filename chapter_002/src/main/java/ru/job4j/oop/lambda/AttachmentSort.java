0461fc4ac5e89a8f0ff80ab3ca0ce0e62ae41273
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-02-18 09:59:08 +0300

    1. Анонимные классы [#214142]

---
+++ chapter_002/src/main/java/ru/job4j/oop/lambda/AttachmentSort.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("1", 100),
+                new Attachment("2", 34),
+                new Attachment("3", 13)
+        );
+
+        Comparator<Attachment> comparatorBySize = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                Attachment left = (Attachment) o1;
+                Attachment right = (Attachment) o2;
+                return left.getSize() - right.getSize();
+            }
+        };
+        attachments.sort(comparatorBySize);
+        System.out.println("comparatorBySize: " + attachments);
+
+        Comparator<Attachment> comparatorByName = new Comparator<Attachment>() {
+            @Override
+            public int compare(Attachment o1, Attachment o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+        attachments.sort(comparatorByName);
+        System.out.println("comparatorByName: " + attachments);
+    }
+}
