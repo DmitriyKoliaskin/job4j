b4e3c10b02c401127439ee2ca0d59e52b1cf3d73
Dmitriy Koliaskin <Hipf02@gamil.com>
2020-03-01 03:14:48 +0300

    3. Уникальность элементов и сортировка.[#155574]

--- chapter_002/src/main/java/ru/job4j/oop/lambda/Profiles.java
+++ chapter_002/src/main/java/ru/job4j/oop/lambda/Profiles.java
@@ -1,10 +1,24 @@
 package ru.job4j.oop.lambda;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+
+    public List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public List<Address> collectSorted(List<Profile> profiles) {
+        return profiles.stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList()
+                );
     }
 }
--- chapter_002/src/test/java/ru/job4j/opp/lambda/ProfilesTest.java
+++ chapter_002/src/test/java/ru/job4j/opp/lambda/ProfilesTest.java
@@ -3,30 +3,48 @@ package ru.job4j.opp.lambda;
 import org.junit.Test;
 import ru.job4j.oop.lambda.Address;
 import ru.job4j.oop.lambda.Profile;
+import ru.job4j.oop.lambda.Profiles;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class ProfilesTest {
+
+    private Profiles profile = new Profiles();
     private List<Profile> list = Arrays.asList(
             new Profile(new Address("Minsk", "Proletarskaya", 43, 12)),
             new Profile(new Address("Serpuhov", "Mosvovskoe highway", 124, 25)),
+            new Profile(new Address("Serpuhov", "Mosvovskoe highway", 124, 25)),
             new Profile(new Address("Moscow", "Pobedy", 62, 624)),
+            new Profile(new Address("Moscow", "Pobedy", 62, 624)),
+            new Profile(new Address("Chehov", "Pushkina", 33, 67)),
             new Profile(new Address("Chehov", "Pushkina", 33, 67))
     );
 
     @Test
     public void whenAdd() {
-        List<Address> result = list.stream().map(Profile::getAddress).collect(Collectors.toList());
+        List<Address> result = profile.collect(list);
         List<Address> expect = List.of(
                 new Address("Minsk", "Proletarskaya", 43, 12),
                 new Address("Serpuhov", "Mosvovskoe highway", 124, 25),
                 new Address("Moscow", "Pobedy", 62, 624),
                 new Address("Chehov", "Pushkina", 33, 67)
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortedByCity() {
+        List<Address> result = profile.collectSorted(list);
+        List<Address> expect = List.of(
+                new Address("Chehov", "Pushkina", 33, 67),
+                new Address("Minsk", "Proletarskaya", 43, 12),
+                new Address("Moscow", "Pobedy", 62, 624),
+                new Address("Serpuhov", "Mosvovskoe highway", 124, 25)
         );
         assertThat(result, is(expect));
     }
