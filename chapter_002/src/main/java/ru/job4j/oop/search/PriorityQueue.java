6301e3fc46e65282fc48982f84805997a291996c
Hipf02@gmail.com <E5a5ee11!>
2021-04-13 18:52:59 +0300

    Очередь с приоритетом на LinkedList [#155678] v0.2

--- chapter_002/src/main/java/ru/job4j/oop/search/PriorityQueue.java
+++ chapter_002/src/main/java/ru/job4j/oop/search/PriorityQueue.java
@@ -8,10 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (var element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
-            } else if (element.getPriority() > task.getPriority()){
+            if (element.getPriority() > task.getPriority()) {
                 break;
+            } else {
+                index++;
             }
         }
         this.tasks.add(index, task);
