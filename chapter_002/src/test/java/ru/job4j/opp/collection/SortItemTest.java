e6b2e849bb064ba0d30be01ab8572bf3f88ae080
Hipf02@gmail.com <E5a5ee11!>
2021-04-18 22:42:20 +0300

    0. Видео [#155671] v0.2

--- chapter_002/src/test/java/ru/job4j/opp/collection/SortItemTest.java
+++ chapter_002/src/test/java/ru/job4j/opp/collection/SortItemTest.java
@@ -13,19 +13,19 @@ import ru.job4j.oop.tracker.SortItemIncrease;
 
 public class SortItemTest {
 
-    public List<Item> createdListItem() {
-        Item item1 = new Item("Aeboy");
-        Item item2 = new Item("Kolobok");
-        Item item3 = new Item("Lisa");
-
-        return Arrays.asList(item1, item2, item3);
-    }
-
-    public void setID(List<Item> list) {
-        list.get(0).setId("1");
-        list.get(1).setId("2");
-        list.get(2).setId("3");
-    }
+//    public List<Item> createdListItem() {
+//        Item item1 = new Item("Aeboy");
+//        Item item2 = new Item("Kolobok");
+//        Item item3 = new Item("Lisa");
+//
+//        return Arrays.asList(item1, item2, item3);
+//    }
+//
+//    public void setID(List<Item> list) {
+//        list.get(0).setId("1");
+//        list.get(1).setId("2");
+//        list.get(2).setId("3");
+//    }
 
     public ArrayList<String> getName(List<Item> list) {
         ArrayList<String> copy = new ArrayList<>();
@@ -37,15 +37,32 @@ public class SortItemTest {
 
     @Test
     public void whenCompareThenDown() {
-        List<Item> list = createdListItem();
-        setID(list);
+        Item item1 = new Item("Aeboy");
+        Item item2 = new Item("Kolobok");
+        Item item3 = new Item("Lisa");
+
+        List<Item> list = Arrays.asList(item1, item2, item3);
+
+        list.get(0).setId("1");
+        list.get(1).setId("2");
+        list.get(2).setId("3");
+
         list.sort(new SortItemDecrease());
-        assertThat(getName(list), is(List.of("Lisa","Kolobok","Aeboy")));
+        assertThat(getName(list), is(List.of("Lisa", "Kolobok", "Aeboy")));
     }
+
     @Test
     public void whenCompareThenRise() {
-        List<Item> list = createdListItem();
-        setID(list);
+        Item item1 = new Item("Aeboy");
+        Item item2 = new Item("Kolobok");
+        Item item3 = new Item("Lisa");
+
+        List<Item> list = Arrays.asList(item1, item2, item3);
+
+        list.get(0).setId("1");
+        list.get(1).setId("2");
+        list.get(2).setId("3");
+
         list.sort(new SortItemIncrease());
         assertThat(getName(list), is(List.of("Aeboy", "Kolobok", "Lisa")));
     }
